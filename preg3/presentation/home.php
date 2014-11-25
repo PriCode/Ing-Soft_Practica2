b4b270895c597cbc69cbde831d90c6513aa571df
ehuarotop <edu.huaroto@gmail.com>
2014-11-24 22:10:39 -0500

    vista inicial

---
+++ preg3/presentation/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="utf-8" />
+        <title>Miraflores Park - Parking Flow Control</title>
+
+        <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen">
+    </head>
+    <body>
+        <div class="container">
+
+            <ul id="nav">
+                <li><a href="#"><img src="images/home.png" /> Home</a></li>
+                <li><a href="#"><span><img src="images/top1.png" /> Registro Autos</span></a>
+                    <div class="subs">
+                        <div class="col">
+                            <ul>
+                                <li><a href="#"><img src="images/bub.png" /> Link 1</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 2</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 3</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#"><span><img src="images/top2.png" /> Generación de Reportes</span></a>
+                    <div class="subs">
+                        <div class="col">
+                            <ul>
+                                <li><a href="#"><img src="images/bub.png" /> Link 1</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 2</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 3</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 4</a></li>
+                                <li><a href="#"><img src="images/bub.png" /> Link 5</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#"><img src="images/top3.png" /> Exportar </a></li>
+                <li><a href="#"><img src="images/top4.png" /> Anotaciones</a></li>
+                <li><a href="#"><img src="images/video_camara.png" /> Monitorear Video Cámaras</a></li>
+
+
+                <h1>Cajero: Pedro Muñoz</h1>
+                <h2>Codigo: ASD765</h2>
+
+
+
+            </ul>
+
+            <div>
+                <h1>Cajero: Pedro Muñoz</h1>
+                <h2>Codigo: ASD765</h2>
+            </div>
+
+        </div>
+
+        <footer>
+            <h2>Miraflores Park</h2>
+            <a href="http://www.script-tutorials.com/css3-metal-menu-with-icons" class="stuts">Flow Car Control System <span>Car Park</span></a>
+        </footer>
+    </body>
+</html>

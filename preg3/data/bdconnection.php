bf0f220b8c40c4fffb5e819d429ad177a266fedb
ehuarotop <edu.huaroto@gmail.com>
2014-11-25 15:34:18 -0500

    mongodb connection

--- preg3/data/bdconnection.php
+++ preg3/data/bdconnection.php
@@ -1,24 +1,8 @@
 <?php
-/*class connection{
-	private $server = "localhost";
-	private $user = "root";
-	private $pass = ".-dogzilla.-";
-	private $db = "miraflorespark";
-
-	function conectar(){
-		return mysql_connect($this->server,$this->user,$this->pass);
-	}
-
-	function consultar($query){
-		$connection = $this->conectar();
-		mysql_select_db($this->$db, $connection);
-		return mysql_query($query,$connection);
-	}
-}*/
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$m = new MongoClient();
+/*$m = new MongoClient();
 $db = $m->selectDB('mensajes');
 $collection = new MongoCollection($db, 'produce');
 
@@ -36,6 +20,28 @@ echo "Sweet\n";
 $cursor = $collection->find($sweetQuery);
 foreach ($cursor as $doc) {
     var_dump($doc);
+}*/
+
+//http://www.elclubdelprogramador.com/2013/05/08/mongodb-creando-una-conexion-a-mongodb-con-php/
+//http://www.sitepoint.com/mongodb-revisited/
+//http://www.dailymotion.com/video/x2awaa1_curso-de-mongodb-con-php-12-consultas-de-agregacion-y-mapreduce_school
+//http://php.net/manual/en/class.mongoclient.php
+
+
+try{
+	$mongo = new MongoClient(); //creamos una conexion a MongoDB
+	$db = $mongo ->miraflorespark;
+	$collection = $db->createCollection("cajeros");
+	$cajero = array("nombre" => "Pablo marmol", "cod_cajero" => "MP3976" );
+	$collection->insert($cajero);
+	echo "Cajero Ingresado";
+  //$databases = $mongo->listDBs(); //Listamos las bases de datos
+  //echo '<pre>';
+  //print_r($databases);
+  	$mongo->close();
+}catch(MongoConnectionException $e) {
+  // manejamos los errores
+  die($e->getMessage());
 }
 
 ?>

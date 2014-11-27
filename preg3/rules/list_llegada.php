66542a8db81f53001975244470de06090bead180
ehuarotop <edu.huaroto@gmail.com>
2014-11-27 00:53:39 -0500

    preg3

--- preg3/rules/list_llegada.php
+++ preg3/rules/list_llegada.php
@@ -1,8 +1,5 @@
 <?php
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
-
 	require_once("../data/bdconnection.php");
 	$conexion = new connection("miraflorespark","llegadas");
 	$conexion->conectar();
